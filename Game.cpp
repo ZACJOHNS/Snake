328667542d3866e9c4d22ad600b912ab477f05ad
Isaac Johnson <me@isaacjohnsononline.com.au>
2014-02-13 11:56:40 +1000

    Completed boilerplate code

--- Game.cpp
+++ Game.cpp
@@ -10,21 +10,42 @@ Game::Game()
 
 void Game::run()
 {
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	while (mWindow.isOpen())
+	{
+		sf::Time elapsedTime = clock.restart();
+		timeSinceLastUpdate += elapsedTime;
+		while (timeSinceLastUpdate > TimePerFrame)
+		{
+			timeSinceLastUpdate -= TimePerFrame;
 
+			processEvents();
+			update(TimePerFrame);
+		}
+
+		render();
+	}
 }
 
 void Game::processEvents()
 {
-
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			mWindow.close();
+	}
 }
 
 void Game::update(sf::Time elapsedTime) 
 {
-
+	// update class
 }
 
 void Game::render() 
 {
-
+	mWindow.clear();
+	mWindow.display();
 }
 
