a9d847c6f4bbb01440ef08adaff2e05f9a3c441c
Isaac Johnson <me@isaacjohnsononline.com.au>
2014-02-13 11:06:01 +1000

    added code

---
+++ Game.h
@@ -0,0 +1,23 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/System/Time.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class Game : private sf::NonCopyable
+{
+public:
+	Game();
+	void run();
+
+private:
+	void processEvents();
+	void update(sf::Time elapsedTime);
+	void render();
+	
+private:
+	static const sf::Time TimePerFrame;
+	sf::RenderWindow mWindow;
+};
+
+#endif
